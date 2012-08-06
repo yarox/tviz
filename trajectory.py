132d20d39554540f186fd676d45298be685ed963
Adrian Martin <adrianmartinparra@gmail.com>
2012-08-06 09:41:54 +0200

    Can move and scale points.
    Added means to move points relative to an offset, and to scale points
    given a factor.

--- trajectory.py
+++ trajectory.py
@@ -4,67 +4,64 @@ trajectory.py
 Draw a trajectory from a given set of 3D coordinates.
 '''
 
-# TODO: Hay que trasladar los puntos hacia el origen.
-#   1. Definir un punto origen.
-#   2. Tomar un punto de la trayectoria como referencia y hacerlo coincidir 
-#      con el origen.         
-#   3. Desplazar el resto de puntos acorde con la referencia. 
-
-# TODO: Hay que escalar los puntos segun una constante.
 # TODO: Dibujar un plano base.
 # TODO: implementarlo como plugin.
 
-#import bpy
+from mathutils import Vector
+import bpy
 import ast
 
 
 
-def get_offset(point, origin):
-    pass
+def moved(point, offset):
+    return point - offset
 
-def move(point, offset):
-    pass
-
-def scale(point, factor):
-    pass
+def scaled(point, factor):
+    return point * factor
     
-def load_points(filename, factor=1):    
+def load_points(filename):    
     with open(filename) as data:
-        return ast.literal_eval(data.read())
-        
-#def draw_trajectory(points, radius=1, type='POLY'): 
-#    objname = 'MyTrajectory'
-#    curvename = 'MyCurve'
-#    
-#    curvedata = bpy.data.curves.new(name=curvename, type='CURVE')
-#    curvedata.dimensions = '3D'
-#    
-#    objectdata = bpy.data.objects.new(objname, curvedata)
-#    objectdata.location = (0, 0, 0)
-#    bpy.context.scene.objects.link(objectdata)
-#    
-#    polyline = curvedata.splines.new(type)
-#    polyline.points.add(len(points) - 1)
-#
-#    for i, point in enumerate(points):
-#        polyline.points[i].co = point + (1, )
-#    
-#    polyline.order_u = len(polyline.points) - 1  
-#    polyline.use_endpoint_u = True
-#    
-#    bpy.ops.curve.primitive_bezier_circle_add()
-#    circle = bpy.context.scene.objects.active
-#    circle.scale = [radius] * 3
-#    
-#    path = bpy.data.objects[objectdata.name]
-#    path.data.bevel_object = circle
-#    
-#    bpy.context.scene.objects.unlink(circle)
+        points = ast.literal_eval(data.read())
+        return [Vector(p) for p in points]
+
+def draw_trajectory(points, radius=0.075, type='POLY'): 
+    objname = 'MyTrajectory'
+    curvename = 'MyCurve'
+    
+    curvedata = bpy.data.curves.new(name=curvename, type='CURVE')
+    curvedata.dimensions = '3D'
+    
+    objectdata = bpy.data.objects.new(objname, curvedata)
+    objectdata.location = (0, 0, 0)
+    bpy.context.scene.objects.link(objectdata)
+    
+    polyline = curvedata.splines.new(type)
+    polyline.points.add(len(points) - 1)
+
+    for i, point in enumerate(points):
+        polyline.points[i].co = point.to_4d()
+    
+    polyline.order_u = len(polyline.points) - 1  
+    polyline.use_endpoint_u = True
+    
+    bpy.ops.curve.primitive_bezier_circle_add()
+    circle = bpy.context.scene.objects.active
+    circle.scale = [radius] * 3
+    
+    path = bpy.data.objects[objectdata.name]
+    path.data.bevel_object = circle
+    
+    bpy.context.scene.objects.unlink(circle)
 
 
-if __name__ == '__main__'
-# Load the point list
-points = load_points('/Users/adri/Documents/Sandbox/blender/data.txt')
+if __name__ == '__main__':
+    # Load the point list
+    points = load_points('/Users/adri/Documents/Sandbox/tviz/data.txt')
+    
+    # Move and scale the original points
+    pivot = 0
+    offset = moved(points[pivot], Vector([0, 0, 0]))
+    points = [scaled(moved(p, offset), 0.05) for p in points]
 
-# Draw the trajectory
-draw_trajectory(points)
+    # Draw the trajectory
+    draw_trajectory(points)
